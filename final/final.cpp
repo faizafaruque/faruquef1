f30d1f46e6cfea70d7f0dbe715c99b22cf19dce4
faizafaruque <36314858+faizafaruque@users.noreply.github.com>
2018-05-16 17:34:58 -0400

    final project

--- final/final.cpp
+++ final/final.cpp
@@ -1,22 +1,46 @@
-#include "tinyxml2.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
-using namespace tinyxml2;
-int main(int argc, const char** argv)
 
+int main()
 {
-	XMLDocument*doc= new XMLDocument();
-	doc-> LoadFile(argv[1]);
-	XMLElement*root=doc->FirstChildElement();
-	cout<<root->FirstChildElement()->Name()<<"\n";
+    string line;
+    ifstream in("package.xml");
 
+    bool begin_tag = false;
+    while (getline(in,line))
+    {
+        std::string tmp; // strip whitespaces from the beginning
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (line[i] == ' ' && tmp.size() == 0)
+            {
+            }
+            else
+            {
+                tmp += line[i];
+            }
+        }
 
+        cout << "-->" << tmp << "<--" << endl;
 
-  XMLElement*root_s=root->FirstChildElement()->NextSiblingElement();
-	cout<<root_s->Name()<<"\n";
+        if (tmp == "<Package>")
+        {
+            cout << "Found <Package>" << endl;
+            begin_tag = true;
+            continue;
+        }
+        else if (tmp == "</Package>")
+        {
+            begin_tag = false;
+            cout << "Found </Package>" << endl;
+        }
 
-
-	return 0;
-
+        if (begin_tag)
+        {
+            cout << tmp << endl;
+        }
+    }
 }
